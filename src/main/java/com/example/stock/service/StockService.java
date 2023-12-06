509f14f7769dd1f11b10bbef4ea322273d647df1
Thomas Sang <40707686+uiseongsang@users.noreply.github.com>
2023-12-06 14:22:38 +0900

    Feat: "Add Decrease Stock Method"
    * stock 조회
    * 재고 감소
    * 갱신된 값 저장

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -1,7 +1,9 @@
 package com.example.stock.service;
 
+import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 // 재고를 감소 시키는 서비스
 @Service
@@ -12,5 +14,17 @@ public class StockService {
     // 생성자 주입
     public StockService(StockRepository stockRepository) {
         this.stockRepository = stockRepository;
+    }
+
+    @Transactional
+    public void decrease(Long id, Long quantity) {
+        // stock 조회
+        Stock stock = stockRepository.findById(id).orElseThrow();
+
+        // 재고를 감소
+        stock.decrease(quantity);
+
+        // 갱신된 값을 저장
+        stockRepository.saveAndFlush(stock);
     }
 }
