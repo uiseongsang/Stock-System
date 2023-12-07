047776269fe554720ee860ee1cab5ec8a160463f
Thomas Sang <40707686+uiseongsang@users.noreply.github.com>
2023-12-07 16:39:56 +0900

    Feat: "Test concurrency - 100 request"
    동시에 100개의 요청이 들어 왔을 경우
    100개의 재고가 1번의 요청이 들어온 경우
    최종적으로 0개의 재고가 남는지 테스트
    -> 결론: 실패한다... 동시성 문제 발생

--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -1,11 +1,13 @@
 package com.example.stock.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,7 +42,34 @@ class StockServiceTest {
         // 100 - 1 = 99
         Stock stock = stockRepository.findById(1L).orElseThrow();
 
-        Assertions.assertEquals( 99L, stock.getQuantity());
+        assertEquals( 99L, stock.getQuantity());
 
+    }
+
+    @Test
+    public void 동시에_100개의_요청() throws InterruptedException {
+        // 멀티 스레드 사용
+        int thread_count = 100;
+
+        // 비동기로 실행하는 작업을 단순하게 사용하게 도와주는 API
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+        // 100개의 요청을 끝날 떄까지 기달려햐하므로 활용
+        CountDownLatch latch = new CountDownLatch(thread_count);
+
+        for(int i = 0; i < thread_count; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown(); // 다른 스레드에서 작업이 끝나기 전까지 대기해주는 클래스
+                }
+            });
+        }
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        assertEquals(0,stock.getQuantity());
     }
 }
